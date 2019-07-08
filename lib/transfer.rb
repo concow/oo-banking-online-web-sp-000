f8d24fc8d9345abe98c923f59bf58334993e44e2
Connor Cowan <ccowan003@comcast.net>
2019-07-08 22:57:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if @status = "complete"           #condition if transaction was previously completed
+    if @status == "complete"           #condition if transaction was previously completed
       @sender.balance += @amount      #reverse @amount(s)
       @receiver.balance -= @amount
       @status = "reversed"            #update @status to reversed
